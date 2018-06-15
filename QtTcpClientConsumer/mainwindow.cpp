a58be4d8d9703b93a5d5859c246254b9f72b8f3a
LislenyTorquato <38656216+LislenyTorquato@users.noreply.github.com>
2018-06-15 08:34:59 -0300

    Add files via upload

---
+++ QtTcpClientConsumer/mainwindow.cpp
@@ -0,0 +1,76 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QDateTime>
+
+MainWindow::MainWindow(QWidget *parent) :
+  QMainWindow(parent),
+  ui(new Ui::MainWindow)
+{
+  ui->setupUi(this);
+  socket = new QTcpSocket(this);
+  connect(ui->pushButtonConnect,
+          SIGNAL(clicked(bool)),
+          this,
+          SLOT (tcpConnect()));
+  connect(ui->pushButtonDisconnect,
+          SIGNAL(clicked(bool)),
+          this,
+          SLOT (tcpDisconnect()));
+
+  connect(ui->pushButtonGet,
+          SIGNAL(clicked(bool)),
+          this,
+          SLOT(getData()));
+}
+
+void MainWindow::tcpConnect(){
+  socket->connectToHost(ui->lineEditIPdoServidor->text(),1234);
+  if(socket->waitForConnected(3000)){
+    qDebug() << "Connected";
+  }
+  else{
+    qDebug() << "Disconnected";
+  }
+}
+
+void MainWindow::tcpDisconnect()
+{
+    socket->disconnectFromHost();
+}
+
+
+
+void MainWindow::getData(){
+  QString str;
+  QByteArray array;
+  QStringList list;
+  qint64 thetime;
+  qDebug() << "to get data...";
+  if(socket->state() == QAbstractSocket::ConnectedState){
+    if(socket->isOpen()){
+      qDebug() << "reading...";
+      socket->write("get 127.0.0.1 5\r\n");
+      socket->waitForBytesWritten();
+      socket->waitForReadyRead();
+      qDebug() << socket->bytesAvailable();
+      while(socket->bytesAvailable()){
+        str = socket->readLine().replace("\n","").replace("\r","");
+        list = str.split(" ");
+        if(list.size() == 2){
+          bool ok;
+          str = list.at(0);
+          thetime = str.toLongLong(&ok);
+          str = list.at(1);
+          qDebug() << thetime << ": " << str;
+        }
+      }
+    }
+  }
+}
+
+
+MainWindow::~MainWindow()
+{
+  delete socket;
+  delete ui;
+}
---
+++ QtTcpClientConsumer/mainwindow.h
@@ -0,0 +1,30 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QTcpSocket>
+#include <QDebug>
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+  Q_OBJECT
+
+public:
+  explicit MainWindow(QWidget *parent = 0);
+  ~MainWindow();
+  
+
+public slots:
+  void getData();
+   void tcpConnect();
+   void tcpDisconnect();
+private:
+  Ui::MainWindow *ui;
+  QTcpSocket *socket;
+};
+
+#endif // MAINWINDOW_H
